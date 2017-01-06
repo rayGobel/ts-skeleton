9cef99ca53b7249c248380ca1796ab0281d9273f
Raynal R. Gobel <raynal.gobel@bhinneka.com>
2017-01-06 16:48:30 +0700

    Add gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,33 @@
 "use strict";
-let gulp = require("gulp");
-let ts = require("gulp-typescript");
-let tsProject = ts.createProject("tsconfig.json");
-let mocha = require("gulp-mocha");
+let gulp = require('gulp');
+let del = require('del');
+let ts = require('gulp-typescript');
+let tsProject = ts.createProject('tsconfig.json');
+let mocha = require('gulp-mocha');
 
-gulp.task("compile", function () {
+gulp.task('clean', function () {
+  return del([
+    'dist/**/*',
+    'dist/'
+  ]);
+});
+
+gulp.task('compile', ['clean'], function () {
   return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest("dist"));
 
 });
 
-gulp.task("mocha-test", ["compile"], function () {
-  gulp.src("dist/test/**/*.js", {read: false})
+gulp.task('mocha-test', ['compile'], function () {
+  gulp.src('dist/test/**/*.js', {read: false})
     .pipe(mocha());
 });
 
-gulp.task("default", ["mocha-test"]);
+gulp.task('default', ['mocha-test']);
 
+gulp.task('monitor', ['mocha-test'], function() {
 
-gulp.task("monitor", ["mocha-test"], function() {
-
-  gulp.watch("src/**/*.ts", ["mocha-test"]);
+  gulp.watch('src/**/*.ts', ['mocha-test']);
 
 });
